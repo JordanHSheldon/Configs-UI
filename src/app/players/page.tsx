64d02c9ee0930220fbefb34995416f8a34dd1f49
jordansheldon <Jordanhsheldon@gmail.com>
2024-07-01 16:22:16 -0400

    added user page, and adjusted layout of other pages

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -5,6 +5,7 @@ import { User } from "../lib/definitions";
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import Grid from "@mui/material/Grid";
+import Link from "next/link";
 
 export default function Page() {
   const pagination = 10;
@@ -58,29 +59,29 @@ export default function Page() {
   };
 
   return (
-    <>
+    <div>
     <br />
       {data ? (
         <>
         <Grid container justifyContent={"center"}  alignItems="center">
           {data.map((user) => (
             <Grid container alignItems="center" spacing={0} direction="column" key={user.id}>
-              <h2>{user?.firstName} {user?.userName} {user?.lastName}</h2>
+              <h2>{user?.firstName}</h2> <Link href={"/user/"+user?.userName}>{user?.userName}</Link> <h2>{user?.lastName}</h2>
             </Grid>
           ))}
           <br/>
           </Grid>
+          <br />
           <Grid container spacing={0} justifyContent={"center"}>
             <span>
-              <button onClick={handlePrevious} disabled={offset === 0}><KeyboardArrowLeftIcon/> Prev </button> 
-              <button onClick={handleNext}>Next <ChevronRightIcon /></button>
+              <button onClick={handlePrevious} disabled={offset === 0}><KeyboardArrowLeftIcon/></button>
+              <button onClick={handleNext}><ChevronRightIcon /></button>
             </span>
           </Grid>
         </>
       ) : (
         <p>No data available.</p>
       )}
-
-    </>
+      </div>
   );
 }
