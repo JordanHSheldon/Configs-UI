11246734ffdf4edf6bf24b78213e5057a37c88df
jordansheldon <Jordanhsheldon@gmail.com>
2024-09-05 10:36:55 -0400

    made some more style changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,26 @@
 "use client"
 
 import Head from "next/head";
-import Link from "next/link";
 
 export default function Page() {
   return (
     <div className={"container"}>
       <Head>
-        <title>configs.cc - Home</title>
+        <title>configs</title>
         <meta name="description" content="Welcome to configs.cc - Home of Player Configurations" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
       <main className={"main"}>
         <h1 className={"title"}>configs</h1>
-        <p className={"description"}>The ultimate destination for player configurations.</p>
-
+        <p className={"description"}>Destination for your configurations</p>
         <p className={"details"}>
           Ever had your config get messed up or updated when you didn't want it to? 
           With Configs, you always have a backup of any configuration you upload.
-          You can also check out other players configurations, check it out below!</p>
+          You can also check out other players configurations, check it out below!
+        </p>
         <div className={"cta"}>
-            <a href="/players" className={"ctaButton"}>Check out other players</a>
+            <a href="/players" className={"ctaButton"}>See players</a>
         </div>
         <br />
         <hr />
