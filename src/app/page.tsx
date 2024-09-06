580ba48ce8afbc29ddd2da84b89b122956262219
jordansheldon <Jordanhsheldon@gmail.com>
2024-09-06 17:55:05 -0400

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,24 @@
 export default function Page() {
   return (
     <div className={"container"}>
-        <title>configs</title>
-        <meta name="description" content="Welcome to configs.cc - Home of Player Configurations" />
-        <link rel="icon" href="/favicon.ico" />
-        <main className={"main"}>
-          <h1 className={"title"}>configs</h1>
-          <p className={"description"}>Destination for your configurations</p>
-          <p className={"details"}>
-            Ever had your config get messed up or updated when you didn't want it to? 
-            With Configs, you always have a backup of any configuration you upload.
-            You can also check out other players configurations, check it out below!
-          </p>
-          <div className={"cta"}>
-              <a href="/players" className={"ctaButton"}>See all configs</a>
-          </div>
-          <p className={"details"}>
-            This is a project made by @jordanhsheldon on github.
-          </p>
-        </main>
+      <title>configs</title>
+      <meta name="description" content="Welcome to configs.cc - Home of Player Configurations" />
+      <link rel="icon" href="/favicon.ico" />
+      <main className={"main"}>
+        <h1 className={"title"}>configs</h1>
+        <p className={"description"}>Destination for your configurations</p>
+        <p className={"details"}>
+          Ever had your config get messed up or updated when you didn't want it to? 
+          With Configs, you always have a backup of any configuration you upload.
+          You can also check out other players configurations, check it out below!
+        </p>
+        <div className={"cta"}>
+            <a href="/players" className={"ctaButton"}>See all configs</a>
+        </div>
+        <p className={"details"}>
+          This is a project made by @jordanhsheldon on github.
+        </p>
+      </main>
     </div>
   );
 }
